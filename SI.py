8ed5112b9b73e7f0c7b2da903ca37348ad20247f
Urvashi Mishra <urvashimishra338@gmail.com>
2023-01-30 11:41:39 +0530

    first commit

---
+++ SI.py
@@ -0,0 +1,10 @@
+# SI CALCULATOR
+p = 10000
+r = 3
+t = 15
+s = p * r * t / 100
+print(f'Principle is ${p}')
+print(f'Rate is {r}%')
+print(f'Time is {t}yrs')
+print('---'*10)
+print(f'Simple Interest is {s}')
